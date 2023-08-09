de3e6e4119a6448df4d72d0b13c32a5189af5349
sanlyshi <sanlyshi@163.com>
2023-08-09 17:18:11 +0800

    feat:it's me

---
+++ vite-ts-react/src/components/main.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "antd";
+function Main() {
+  const [name] = useState("sanlyshi");
+  return (
+    <div>
+      <h3>
+        wellcome to {name}'s place,please click
+        <span className="mg10">
+          <Link to="/sanlyshi-info">
+            <Button>here</Button>
+          </Link>{" "}
+        </span>
+        to see him !
+      </h3>
+    </div>
+  );
+}
+export default Main;
---
+++ vite-ts-react/src/components/sanlyshi-info.tsx
@@ -0,0 +1,10 @@
+import pyy from "../assets/pyy.png";
+function Sanlyshi() {
+  return (
+    <div>
+      <img className="pyy_pic" src={pyy} alt="" />
+      <div>Yes,It's me.</div>
+    </div>
+  );
+}
+export default Sanlyshi;
